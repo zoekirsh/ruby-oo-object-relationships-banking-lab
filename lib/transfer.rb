ba4c0285816dafaa1c82905b7ce98a7f3541527d
Alena Trushnikova <70681270+AlenaTrushnikova@users.noreply.github.com>
2020-11-19 11:04:22 -0800

    Update transfer.rb

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,6 +1,30 @@
 require 'pry'
 require_relative '../lib/bank_account'
-
 class Transfer
   # your code here
+  attr_reader :sender, :receiver, :amount
+  attr_accessor :status
+
+  def initialize (sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @status = 'pending'
+    @amount = amount
+  end
+
+  def valid?
+    sender.valid? && receiver.valid?
+  end
+
+  def execute_transaction
+    if valid? && sender.balance > amount && self.status == 'pending'   
+      sender.balance -= @amount
+      receiver.balance += @amount
+      self.status = 'complete'
+    else
+      self.status = 'rejected' 
+      "Transaction rejected. Please check your account balance."
+    end 
+  end
+
 end
